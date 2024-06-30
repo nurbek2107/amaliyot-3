729fd891d6da329d55a4015b35c20d1711493626
Nurbek <161306161+nurbek2107@users.noreply.github.com>
2024-06-30 23:55:51 +0500

    main
    Signed-off-by: Nurbek <161306161+nurbek2107@users.noreply.github.com>

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEHwnxz99EzQKgQJ810HxYpXPrBwAScF4",
-  authDomain: "mymarket-e0861.firebaseapp.com",
-  projectId: "mymarket-e0861",
-  storageBucket: "mymarket-e0861.appspot.com",
-  messagingSenderId: "357483405494",
-  appId: "1:357483405494:web:31f278e229e72d1042c617",
-  measurementId: "G-H0PEBKQ1QS"
+  apiKey: "AIzaSyD9pLhwbs7BARU0hVrGcpd6_IRefgP6AQs",
+  authDomain: "amaliyot-d84ef.firebaseapp.com",
+  projectId: "amaliyot-d84ef",
+  storageBucket: "amaliyot-d84ef.appspot.com",
+  messagingSenderId: "599218497574",
+  appId: "1:599218497574:web:476a87180458838189dee7",
+  measurementId: "G-GRH2Q71CTY"
 };
 
 const app = initializeApp(firebaseConfig);
